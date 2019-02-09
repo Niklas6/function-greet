bf4f66949377ae2cdd8a71fb015bfb34905ab605
Niklas <nikkno@t-online.de>
2019-02-09 11:00:14 -0600

    First commit

---
+++ function greet/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  function greet
+//
+//  Created by Knobel, Niklas (MU-Student) on 2/9/19.
+//  Copyright © 2019 Knobel, Niklas (MU-Student). All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
